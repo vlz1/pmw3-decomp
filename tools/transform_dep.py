a64005306d954cb395e0e259e5a9215725f8129a
vlz1 <122755709+vlz1@users.noreply.github.com>
2026-09-10 01:15:05 -0500

    See if transform_dep.py from https://github.com/dbalatoni13/nfsmw/blob/main/tools/transform_dep.py fixes the depfiles

--- tools/transform_dep.py
+++ tools/transform_dep.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# Taken from the NSFMW decomp:
+# https://github.com/dbalatoni13/nfsmw/blob/main/tools/transform_dep.py
+
 ###
 # Transforms .d files, converting Windows paths to Unix paths.
 # Allows usage of the mwcc -MMD flag on platforms other than Windows.
@@ -13,6 +16,7 @@
 
 import argparse
 import os
+import re
 from platform import uname
 
 wineprefix = os.path.join(os.environ["HOME"], ".wine")
@@ -27,6 +31,7 @@ def in_wsl() -> bool:
 
 def import_d_file(in_file: str) -> str:
     out_text = ""
+    build_root = os.getcwd()
 
     with open(in_file) as file:
         for idx, line in enumerate(file):
@@ -42,19 +47,26 @@ def import_d_file(in_file: str) -> str:
                     path = line.lstrip()[:-3]
                 else:
                     path = line.strip()
-                # lowercase drive letter
-                path = path[0].lower() + path[1:]
-                if path[0] == "z":
-                    # shortcut for z:
-                    path = path[2:].replace("\\", "/")
-                elif in_wsl():
-                    path = path[0:1] + path[2:]
-                    path = os.path.join("/mnt", path.replace("\\", "/"))
+                path = path.replace("\\", "/")
+                if re.match(r"^[A-Za-z]:", path):
+                    # lowercase drive letter for Windows-style absolute paths
+                    path = path[0].lower() + path[1:]
+                    if path[0] == "z":
+                        # shortcut for z:
+                        path = path[2:]
+                    elif in_wsl():
+                        path = path[0:1] + path[2:]
+                        path = os.path.join("/mnt", path)
+                    else:
+                        # use $WINEPREFIX/dosdevices to resolve path
+                        path = os.path.realpath(os.path.join(winedevices, path))
+                elif os.path.isabs(path):
+                    path = os.path.realpath(path)
                 else:
-                    # use $WINEPREFIX/dosdevices to resolve path
-                    path = os.path.realpath(
-                        os.path.join(winedevices, path.replace("\\", "/"))
-                    )
+                    # ProDG often emits repo-relative includes like src\Foo.h.
+                    # Keep them anchored to the current build root instead of
+                    # incorrectly treating them as Wine drive roots.
+                    path = os.path.realpath(os.path.join(build_root, path))
                 out_text += "\t" + path + suffix + "\n"
 
     return out_text
